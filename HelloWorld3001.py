14be506da04bd7c30c2c095bcd1f44c5643fadde
holguinb <44814019+holguinb@users.noreply.github.com>
2018-11-06 10:21:55 -0700

    Add files via upload

---
+++ HelloWorld3001.py
@@ -0,0 +1,11 @@
+print("Hello World")
+var = 2018
+print("Today is Monday the 5th of November ", var )
+
+print(7 + 8)
+print(7 - 8)
+print(7*8)
+print(50/5)
+print(5**2)
+
+print("My name is Bryan Holguin  \n"* 5)
